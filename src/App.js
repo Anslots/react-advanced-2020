8106fa92bcf63a27ea81bd89cfcb550ee12859bf
Andile Selotso <mellismith72@gmail.com>
2022-03-31 15:27:18 -0400

    able to set reset button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/4-useState-object";
-// import Final from "./tutorial/1-useState/final/3-useState-array";
+import Setup from "./tutorial/1-useState/setup/5-useState-counter";
 function App() {
   return (
     <div className="container">
       <Setup />
       {/* <Final /> */}
     
-     
+
     </div>
   );
 }
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] =useState({
+    name: 'Peter',
+    age: 25,
+    message:'random message',
+  });
+  const [name,setName] = useState('Peter')
+  const [age,setAge] = useState(24)
+  const[message, setMessage] = useState('random message')
+    ;
+const changeMessage = () => {
+  //setPerson({...person, message:'hello world'});
+  setMessage ('hello world');
+};
+ return(
+  <>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h4>{message}</h4>
+  <button className='btn' onClick={changeMessage}>
+    change message
+  </button>
+  </>
+
+)
 };
 
 export default UseStateObject;
